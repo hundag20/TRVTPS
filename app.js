c64ee6493054103630e5c82458182dd0dd2f31d1
Hunda Guluma <hundaguluma@gmail.com>
2022-03-26 16:50:30 +0300

    app test

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const driverRoutes = require('./routes/driverRoute');
+const officerRoutes = require('./routes/officerRoute');
+const adminRoutes = require('./routes/adminRoute');
+const payRoutes = require('./routes/payRoute');
+//const adminRoutes = require('./routes/admin');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+    console.log('/ get received');
+    res.send('<html>jjj</html>');
 });
-server.listen(3000);
+app.use(driverRoutes);
+app.use(officerRoutes);
+app.use(adminRoutes);
+app.use(express.static(__dirname));
+
+app.listen();
+
+// var http = require('http');
+// var server = http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     var message = 'It works!\n',
+//         version = 'NodeJS ' + process.versions.node + '\n',
+//         response = [message, version].join('\n');
+//     res.end(response);
+// });
+// server.listen(3000);
