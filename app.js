04b48fb4cd31f7c0e07ef7d710c88d931f327b12
Hunda Guluma <hundaguluma@gmail.com>
2022-03-29 11:43:53 +0300

    app

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const payRoutes = require('./routes/payRoute');
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/', (req, res) => {
+app.get('/ts', (req, res) => {
     console.log('/ get received');
     myLogger.log('/ get received');
     res.send('<html>jjj</html>');
