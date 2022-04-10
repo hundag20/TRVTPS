1cb001598cd86787eb85ea60bbdc59c35df3f0e4
Hunda Guluma <hundaguluma@gmail.com>
2022-04-11 00:33:13 +0300

    ussd at trvtps.etmilestone.com

--- routes/payRoute.js
+++ routes/payRoute.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // home
 router.get('/ts/pay/init', paymentController.checkoutExpress);
-router.post('/ts/pay/ipn', paymentController.IPNDestination);
+//router.post('/ts/pay/ipn', paymentController.IPNDestination);
 router.get('/ts/pay/success', paymentController.successDestination);
 // router.get('/pay/success', officerController.addMultiUsers);
 // router.get('/pay/ipn', officerController.addMultiUsers);
