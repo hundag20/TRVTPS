92935f5de280be96eea88995505e178d70a8a1df
Hunda Guluma <hundaguluma@gmail.com>
2022-05-29 15:23:08 +0300

    get record feature added to officer endpoints

--- routes/officerRoute.js
+++ routes/officerRoute.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 
 const officerController = require("../controller/officerController");
+const driverController = require("../controller/driverController");
 const penaltyController = require("../controller/penaltyController");
 const auth = require("../middleware/auth.js");
 
@@ -44,6 +45,32 @@ router.get(
     } else next();
   },
   penaltyController.issueTicket
+);
+
+//get record endpoint
+router.get(
+  "/ts/officer/retRecord",
+  auth.verifyToken,
+  //verify role is driver
+  (req, res, next) => {
+    if (req.role != "officer") {
+      res.send({
+        status: 400,
+        error: "you don't have officer authorization",
+      });
+    } else {
+      if (req.query.license_id) {
+        req.uname = req.query.license_id;
+        next();
+      } else {
+        return res.status(400).send({
+          status: 400,
+          message: "license id not provided",
+        });
+      }
+    }
+  },
+  driverController.getRecord
 );
 
 //issue ticket endpoint
